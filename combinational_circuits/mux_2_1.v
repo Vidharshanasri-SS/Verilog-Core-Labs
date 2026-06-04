91fd1c16fa0563ba53ea42534df18ed83ff23714
Vidharshana Sri S S <vidharshanasri.ss@gmail.com>
2026-06-04 17:35:49 +0530

    Add files via upload

---
+++ combinational_circuits/mux_2_1.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 18.05.2026 17:36:46
+// Design Name: 
+// Module Name: mux_2_1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+module mux_2_1(out,s1,s2,a,b,c,d);
+output reg out;
+input a,b,c,d,s1,s2;
+always @ (s1 or s2)
+begin 
+case({s1,s2})
+2'b00:out=a; 
+2'b01:out=b;
+2'b10:out=c;
+default:out=d;
+endcase
+end
+endmodule
