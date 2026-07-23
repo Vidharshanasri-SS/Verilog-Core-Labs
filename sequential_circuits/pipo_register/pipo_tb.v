1b34bbdf38ed8a2c32fe812843b79e7bfd1abae9
Vidharshana Sri S S <vidharshanasri.ss@gmail.com>
2026-07-23 12:05:39 +0530

    Add files via upload

---
+++ sequential_circuits/pipo_register/pipo_tb.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02.06.2026 17:34:21
+// Design Name: 
+// Module Name: pipo_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pipo_tb;
+wire [3:0]y;
+reg clk;
+reg [3:0]i;
+
+endmodule
